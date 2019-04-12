f239dcb7dfe15a2fe7a739a1be99b9a6418b3e79
abbenham <newcratie@gmail.com>
2019-04-12 18:56:33 +0200

    websocket

---
+++ api/chat_test.go
@@ -0,0 +1,13 @@
+package api
+
+import (
+	bolt "github.com/johnnadratowski/golang-neo4j-bolt-driver"
+	"os"
+	"testing"
+)
+
+func TestWebSocket(t *testing.T) {
+	driver := bolt.NewDriver()
+	host := os.Getenv("NEO_HOST")
+	app.Neo, _ = driver.OpenNeo("bolt://neo4j:secret@" + host + ":7687")
+}
--- api/router.go
+++ api/router.go
@@ -1,6 +1,12 @@
 package api
 
+import (
+	"github.com/gin-gonic/gin"
+	"gopkg.in/olahol/melody.v1"
+)
+
 func (app *App) routerAPI() {
+	m := melody.New()
 	auth := app.R.Group("/auth")
 	{
 		auth.POST("/login", Login)
@@ -10,5 +16,13 @@ func (app *App) routerAPI() {
 	api := app.R.Group("/api")
 	{
 		api.POST("/get_people", GetPeople)
+		api.GET("/ws", func(c *gin.Context) {
+			m.HandleRequest(c.Writer, c.Request)
+		})
+
 	}
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.Broadcast(msg)
+	})
 }
