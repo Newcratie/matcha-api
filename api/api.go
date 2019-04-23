1c9dfd78fd9e7fe31d6c3f8a20bcc2ba8b8ee776
Newcratie <newcratie@gmail.com>
2019-04-23 14:11:01 +0200

    Chek Neo pool before connection

--- api/api.go
+++ api/api.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	bolt "github.com/johnnadratowski/golang-neo4j-bolt-driver"
 	"os"
+	"time"
 )
 
 const hashKey = "5c894d411c2f7445dbadb9b6"
@@ -13,17 +14,30 @@ func (app *App) newApp() {
 	app.R = gin.Default()
 }
 
+func NewConn(host string) (bolt.Conn, error) {
+	DB, err := bolt.NewDriverPool("bolt://neo4j:secret@"+host+":7687", 1000)
+	if err != nil {
+		return nil, err
+	}
+	retries := 0
+	for retries < 300 {
+		conn, _ := DB.OpenPool()
+		if conn != nil {
+			return conn, nil
+		}
+		retries = retries + 1
+		fmt.Println("neo4j not ready, waiting 1s and trying again ", retries)
+		time.Sleep(1 * time.Second)
+	}
+	return nil, err
+}
+
 func Run() {
 	app.newApp()
-	driver := bolt.NewDriver()
 	host := os.Getenv("NEO_HOST")
 
-	var err error
-	app.Neo, err = driver.OpenNeo("bolt://neo4j:secret@" + host + ":7687")
-	fmt.Println(err)
+	app.Neo, _ = NewConn(host)
 
 	go app.routerAPI()
-	app.Db = dbConnect()
-
 	app.R.Run(":81")
 }
--- run.go
+++ run.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"github.com/Newcratie/matcha-api/api"
 )
 
 func main() {
-	fmt.Println("Run Api")
 	api.Run()
-
 }
