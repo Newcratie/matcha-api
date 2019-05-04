820e57a63dccda37981eb51ed2049b2177a15e74
Newcratie <newcratie@gmail.com>
2019-05-04 20:35:31 +0200

    dbInsertNotifications. Need Test

---
+++ api/env.go
@@ -0,0 +1,6 @@
+package api
+
+const (
+	imageSrc  = "/public/img"
+	imageHost = "http://localhost:8080/src" + imageSrc
+)
--- api/notifications.go
+++ api/notifications.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"gopkg.in/olahol/melody.v1"
 	"strconv"
@@ -14,12 +16,39 @@ type Notification struct {
 	SubjectId int64  `json:"subject_id"`
 }
 
-func (app *App) postNotification(n Notification, msg []byte) {
-	id := strconv.FormatInt(n.UserId, 10)
+func (app *App) postNotification(message string, userId, authorId, subjectId int64) {
+	n := Notification{
+		message,
+		0,
+		userId,
+		authorId,
+		subjectId,
+	}
+	msg, _ := json.Marshal(n)
+	id := strconv.FormatInt(34, 10)
 	url := "/api/notifications/websocket/" + id
-	app.M.BroadcastFilter(msg, func(session *melody.Session) bool {
+
+	_ = app.M.BroadcastFilter(msg, func(session *melody.Session) bool {
 		return session.Request.URL.Path == url
 	})
+}
+
+func (app *App) dbInsertNotification(byt []byte) {
+	var dat map[string]interface{}
+	if err := json.Unmarshal(byt, &dat); err != nil {
+		panic(err)
+	}
+	fmt.Println(dat)
+	dat["message"] = dat["message"].(string)
+	dat["author_id"] = int64(dat["author_id"].(float64))
+	dat["user_id"] = int64(dat["user_id"].(float64))
+	dat["subject_id"] = int64(dat["subject_id"].(float64))
+	q := `
+MATCH (a:User)
+WHERE ID(a)={user_id}
+CREATE (a)<-[s:TO]-(n:Notif {message:{message}, author_id: {author_id}, subject_id: {subject_id}})`
+	st := app.prepareStatement(q)
+	executeStatement(st, dat)
 }
 
 func notificationsHistoryHandler(c *gin.Context) {
